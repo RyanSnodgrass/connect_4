aa53be61bef73e1be7c7eac08e4a02ef75825f79
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2017-03-25 20:09:29 +0000

    linted style for entire app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,4 @@
+# Main splash page for the application.
 class WelcomeController < ActionController::Base
   def index; end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,5 @@
+# Single point of entry for all ActiveRecord models. Much like controllers
+# inherit from ApplicationController
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
